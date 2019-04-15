1f81ae7a36e6083d16f8dfb9d92b6bd47c6c10b6
Rutskiy Daniil <dstdfx@gmail.com>
2019-04-15 22:10:03 +0300

    Fix ansible dependency according to CVE-2019-3828
    Vulnerable versions: >= 2.6.0, < 2.6.14
    Patched version: 2.6.14
    Ansible fetch module before versions 2.5.15, 2.6.14, 2.7.8 has
    a path traversal vulnerability which allows copying and overwriting
    files outside of the specified destination in the local ansible
    controller host, by not restricting an absolute path.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     packages=["ansible/modules/selvpc",
               "ansible/module_utils/selvpc_utils"],
     install_requires=[
-        'ansible==2.6.3',
+        'ansible==2.6.14',
         'python-selvpcclient==1.0'
     ],
 )
